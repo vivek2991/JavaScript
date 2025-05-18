d9c6b9db605a3c3a0f10d49937627e240fba19f5
Vivek Patel <vivekpatel2991@gmail.comm>
2025-05-18 16:10:37 +0530

    add few lines

--- domfile.js
+++ domfile.js
@@ -1,2 +1,17 @@
 console.log("My changes commit check");
 console.log("My changes commit check 2");
+
+
+function myFunction() {
+    for (let i = 0; i < 10; i++) {
+        console.log("My  changes commit check 2");
+    }
+    console.log("My changes commit check 3");
+    console.log("My changes commit check 4");
+    console.log("My changes commit check 5");
+    console.log("My changes commit check 6");
+    console.log("My changes commit check 7");
+    console.log("My changes commit check 8");
+    console.log("My changes commit check 9");
+    console.log("My changes commit check 10");
+}
