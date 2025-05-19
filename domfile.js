c165e0fc360dd3b857372dae5015288de0815722
Vivek Patel <vivekpatel2991@gmail.comm>
2025-05-19 15:04:34 +0530

    dom learnings with different elements and attibutes

--- domfile.js
+++ domfile.js
@@ -43,20 +43,68 @@
 
 // h2.innerText = h2.innerText + " from Vivek Patel College";
 
-let divs = document.querySelectorAll(".box");
+// let divs = document.querySelectorAll(".box");
 
-// divs[0].innerText = "New Unique Value1";
-// divs[1].innerText = "New Unique Value2";
-// divs[2].innerText = "New Unique Value3";
+// // divs[0].innerText = "New Unique Value1";
+// // divs[1].innerText = "New Unique Value2";
+// // divs[2].innerText = "New Unique Value3";
 
-let index = 1;
-for(let div of divs){
-    // two ways to add text with + and $
-    //div.innerText = "New Unique Value" + index;
-    div.innerText = `New Unique Value ${index}`;
-    index++;
-} 
+// let index = 1;
+// for(let div of divs){
+//     // two ways to add text with + and $
+//     //div.innerText = "New Unique Value" + index;
+//     div.innerText = `New Unique Value ${index}`;
+//     index++;
+// } 
 
+// let div = document.querySelector("div");
+// console.dir(div);
 
+// let id = div.getAttribute("id");
+// console.log(id);
 
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class"));
+// console.log(para.getAttribute("id"));
 
+// console.log(para.setAttribute("class", "newclass"));
+
+// let div = document.querySelector("div");
+
+// div.style.backgroundColor = "red";
+// div.style.fontSize = "20px";
+// div.innerText = "Hello World";
+
+// let newbtn =  document.createElement("button");
+// newbtn.innerText = "Click Me";
+
+// let div = document.querySelector("div");
+// //div.appendChild(newbtn);
+// //div.prepend(newbtn);
+// //div.before(newbtn);
+// div.after(newbtn);
+
+// let p = document.querySelector("p");
+// p.after(newbtn);
+
+// let newheading = document.createElement("h1");
+// newheading.innerHTML = "<i>Hi, I am New Heading</i>";
+
+// document.querySelector("body").prepend(newheading);
+
+// let rmpara = document.querySelector("p");
+// rmpara.remove();
+
+let newbtn = document.createElement("button");
+newbtn.innerText = "Click Me!";
+
+newbtn.style.backgroundColor = "red";
+newbtn.style.color = "white";
+
+document.querySelector("body").prepend(newbtn);
+
+//Q2
+
+let mypara = document.querySelector("p");
+mypara.getAttribute("class");
+mypara.classList.add("newclass");
